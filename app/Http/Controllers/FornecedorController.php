9a410a27b151e92822605facfa07e5347ec0fde4
Samuel Souza <samuelsouza0802@gmail.com>
2024-05-28 02:12:28 -0300

    Blade - @switch/case

--- app/Http/Controllers/FornecedorController.php
+++ app/Http/Controllers/FornecedorController.php
@@ -14,16 +14,29 @@ class FornecedorController extends Controller
      'status' => 'N', 
      'CNPJ' => '0',
      'RG' => '313131',
+     'DD' => '92', //Manaus (AM)
+     'telefone' => '98400-5764'
    ],
 
      1 => [
       'nome' => 'Fornecedor 1',
      'status' => 'N', 
      'CNPJ' => '00.000.000.00',
+     'RG' => '3188',
+     'DD' => '85', //Fortaleza (CE)
+     'telefone' => '98400-0000'
        ],
 
- ];
+       2 => [
+         'nome' => 'Fornecedor 1',
+        'status' => 'N', 
+        'CNPJ' => '00.000.000.00',
+        'RG' => '3188',
+        'DD' => '32', //luiz de fora (mg)
+        'telefone' => '98400-0000'
+          ],
 
+ ];
 
 
     return view('app.fornecedor.index', compact('fornecedores')); //compact: é utilizavel pra renderizar uma variavel no front
--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -8,20 +8,43 @@
 
 
 @isset($fornecedores)
-Fornecedor: {{ $fornecedores[0]['nome']}}
+Fornecedor: {{ $fornecedores[2]['nome']}}
 <br>
 
-Status: {{ $fornecedores[0]['status']}} 
+Status: {{ $fornecedores[2]['status']}} 
 <br>
 
- CNPJ: {{ $fornecedores[0]['CNPJ'] ?? 'Dado não foi preenchido'}}
+ CNPJ: {{ $fornecedores[2]['CNPJ'] ?? 'Dado não foi preenchido'}}
  <br>
- 
- RG: {{$fornecedores[1]['RG'] ?? 'Rg não foi preenchido!'}}
- {{-- $variavel testada não estiver definida
-    ou
-    $variavel testada possuir o valor null
-    --}}
+
+ RG: {{$fornecedores[2]['RG'] ?? 'Rg não foi preenchido!'}}
+
+ <br>
+
+ DD: {{$fornecedores[2]['DD'] ?? 'DD não foi preenchido!'}}
+
+ <br>
+
+ telefone: ({{$fornecedores[2]['telefone'] ?? ''}}) {{$fornecedores[2]['telefone'] ?? ''}} 
+ <br>
+@switch($fornecedores[2]['telefone'])
+         @case('92')
+           Manaus - AM             
+         @break
+
+         @case('32')
+             juiz de fora - MG
+         @break
+
+         @case(85)
+             Fortaleza - CE
+         @break
+         @default
+
+          Estado não endetificado
+        
+@endswitch
+
 @endisset
 
      
