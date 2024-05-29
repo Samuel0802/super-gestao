6adb068a5acd4552d66d54c673c1828f94e714f7
Samuel Souza <samuelsouza0802@gmail.com>
2024-05-28 22:14:03 -0300

    Blade - @foreach

--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -9,36 +9,31 @@
 
 @isset($fornecedores)
 
-{{-- declarando a variavel $i --}}
-  @php $i = 0; @endphp 
-
-  @while (isset($fornecedores[$i]))
-      
- 
-Fornecedor: {{ $fornecedores[$i]['nome']}}
+{{-- Foreach ele faz uma copia do array original pra não efetar o array original  --}}
+@foreach ($fornecedores as $indice => $fornecedor)
+  
+Fornecedor: {{  $fornecedor['nome']}}
 <br>
 
-Status: {{ $fornecedores[$i]['status']}} 
+Status: {{  $fornecedor['status']}} 
 <br>
 
- CNPJ: {{ $fornecedores[$i]['CNPJ'] ?? 'Dado não foi preenchido'}}
+ CNPJ: {{  $fornecedor['CNPJ'] ?? 'Dado não foi preenchido'}}
  <br>
 
- RG: {{$fornecedores[$i]['RG'] ?? 'Rg não foi preenchido!'}}
-
- <br>
-
- DD: {{$fornecedores[$i]['DD'] ?? 'DD não foi preenchido!'}}
+ RG: {{ $fornecedor['RG'] ?? 'Rg não foi preenchido!'}}
 
  <br>
 
- telefone: ({{$fornecedores[$i]['telefone'] ?? ''}}) {{$fornecedores[$i]['telefone'] ?? ''}} 
+ DD: {{ $fornecedor['DD'] ?? 'DD não foi preenchido!'}}
+
+ <br>
+
+ telefone: ({{ $fornecedor['telefone'] ?? ''}}) {{ $fornecedor['telefone'] ?? ''}} 
  <br>
  <hr>
+ @endforeach
 
- @php $i++ @endphp
- {{-- autoincremento --}}
- @endwhile
 @endisset
 
 
