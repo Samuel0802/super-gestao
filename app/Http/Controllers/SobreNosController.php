1faf220fc9967d16e8b51c897b23a2d28ce36828
samuel souza <samuelsouza0802@gmail.com>
2024-06-23 20:10:01 -0300

    Middleware nos controllers sobrenos

--- app/Http/Controllers/SobreNosController.php
+++ app/Http/Controllers/SobreNosController.php
@@ -3,10 +3,20 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\MotivoContato;
+use App\http\Middleware\LogAcessoMiddleware;
 
 class SobreNosController extends Controller
+
+//Passando Middleware passando no construtor do controller
 {
+ public function __construct(){
+    $this->middleware(LogAcessoMiddleware::class);
+ }
+
   public function SobreNos(){
-    return view('site.sobrenos');
+    $motivo_contatos =  MotivoContato::all();//Recuperar todos os registro do banco no formato de objeto
+
+    return view('site.sobrenos', ['motivo_contatos' => $motivo_contatos]);
   }
 }
